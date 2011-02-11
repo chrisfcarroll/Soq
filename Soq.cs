7a3634ce32112c4285942cbfb8024389b2924046
Chris F Carroll <chris@chrisbook.local>
2011-02-11 00:23:22 +0000

    First commit

---
+++ Soq.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WarringtonSoftwareLtd
+{
+    /// <summary>
+    /// A simple Object Quote class.
+    /// This is intended as a C sharp implementation of the Object Quote Pattern
+    /// See <see cref="http://www.cafe-encounter.net/p157/simple-object-quotation"/>
+    /// </summary>
+    public class Soq
+    {
+        private static Soq instance = new Soq();
+        private static bool isUsed = false;
+
+        public static T Quote<T>(T o)
+        {
+            isUsed = true;
+            return instance.QuoteImplementation<T>(o);
+        }
+
+        public virtual T QuoteImplementation<T>(T o)
+        {
+            return o;
+        }
+
+        public static void ConfigureForTest(Soq testSoq, bool ignoreProductionEnvironmentSafetyLock)
+        {
+            if (isUsed && !ignoreProductionEnvironmentSafetyLock)
+            {
+                throw new InvalidOperationException("The Soq cannot be configured for test if it has already been called once. Which it has.");
+            }
+            instance = testSoq ?? new Soq();
+        }
+    }
+}
---
+++ TestSoq.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using Moq;
+
+namespace WarringtonSoftwareLtd
+{
+    [TestFixture]
+    public class When_a_soq_is_configured_for_test
+    {
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void A_used_soq_should_throw_if_ignore_not_set()
+        {
+            Soq.Quote<Object>(new Object());
+            Soq.ConfigureForTest(new Soq(), false);
+        }
+
+        [Test]
+        public void A_used_soq_should_not_throw_if_ignore_is_set()
+        {
+            Soq.Quote<Object>(new Object());
+            Soq.ConfigureForTest(new Soq(), true);
+        }
+    
+    }
+
+    [TestFixture]
+    public class When_SoqQuote_is_called
+    {
+        [Test]
+        public void An_unconfigured_Soq_should_return_the_passed_object()
+        {
+            //A
+            Soq.ConfigureForTest(null, true);
+            Object o = new DateTime();
+            //A
+            Object result = Soq.Quote<Object>(o);
+            //A
+            Assert.AreSame<Object>(o,result);
+        }
+
+        [Test]
+        public void A_Soq_configured_with_a_moq_should_return_what_the_moq_was_configured_to_return()
+        {
+            //Arrange
+            Object o = "quoted object";
+            Object dummy = DateTime.Now;
+
+            var mockSoq = new Mock<Soq>();
+            mockSoq.Setup(soq => soq.QuoteImplementation<Object>(o)).Returns(()=>dummy);
+            Soq.ConfigureForTest(mockSoq.Object, true);
+
+            // Act
+            Object result = Soq.Quote<Object>(o);
+
+            //Assert
+            Assert.AreSame<Object>(dummy, result);
+        }
+    }
+}
