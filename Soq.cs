6f72cbe0865e0a672df9c45c772a93bc7c0b9591
Chris.Carroll <chris.carroll@unforgettable.me.uk>
2018-03-23 17:24:30 +0000

    Created project so that tests run

--- Soq.cs
+++ Soq.cs
@@ -1,63 +1,56 @@
 ﻿using System;
 
-namespace WarringtonSoftwareLtd
+namespace SoqIt
 {
     /// <summary>
-    /// A simple Object Quote class.
-    /// This is intended as a C sharp implementation of the Object Quote Pattern
-    /// See <see cref="http://www.cafe-encounter.net/p157/object-quotation"/>
+    /// Simple Object Quotation : See <see cref="http://www.cafe-encounter.net/p157/object-quotation" />
     /// </summary>
     public class Soq
     {
-        private static Soq instance = new Soq();
+        static Soq instance = new Soq();
 
-        /// <summary>
-        /// Surround or 'Quote' calls that construct or build your dependencies with this method.
-        /// Example usage: D1 = Soq.It( new SomeDependency() );  
-        /// </summary>
+        /// <summary>Put a 'SoqIt()' round calls that construct your dependencies to make them swappable at Test-time.</summary>
         /// <typeparam name="T">The type of the dependancy interface or class</typeparam>
-        /// <param name="instantiatedDependency">The instantiated instance of a T that should be used in the production runtime environment</param>
-        /// <returns>In a production environment - that is, when you have not called ConfigureForTest - this method is a simple passthrough and will always return <see cref="dependency"/>
-        /// If the Soq has been configured with a MoqSoq, then this method will return the result of calling MoqSoq.QuoteImplementation&lt;T&gt;(dependency)
+        /// <param name="instance">The instantiated T to use in production</param>
+        /// <returns>
+        /// In a production environment - that is, when you have not called <seealso cref="SwapSoqs"/> - this method is a simple
+        /// passthrough and returns <paramref name="instance" />
+        /// If the Soq has been configured with some fake or mock Soq, then this method will return the result of calling
+        /// the virtual method Soq.SwapIt&lt;T&gt;(instance)
         /// </returns>
-        public static T It<T>(T instantiatedDependency)
-        {
-            return instance.QuoteImplementation<T>(instantiatedDependency);
-        }
+        /// <example><code>var someDep = Soq.It( new SomeDependency() );</example>
+        public static T It<T>(T instance) => Soq.instance.SwapIt(instance);
 
         /// <summary>
-        /// Setup your Moq Soq to return your mock dependencies when this method is called.
-        /// Example implementation with Moq as your mock framework : 
-        /// //Arrange  
+        /// Setup your fake or mock Soq to return your mock dependencies when this method is called.
+        /// Example implementation with Moq as your mock framework :
+        /// <code>
+        /// //Arrange
         /// SomeDependency dummy = new Mock&lt;SomeDependency&gt;().Object;
-        /// var mockSoq = new Mock<Soq>();
-        /// mockSoq.Setup(soq => soq.QuoteImplementation&lt;SomeDependency&gt;(It.IsAny&lt;SomeDependency&gt;()))
-        ///        .Returns(dummy);
-        /// Soq.ConfigureForTest(mockSoq.Object);
+        /// var mockSoq = new Mock&lt;Soq&gt;();
+        /// mockSoq.Setup(soq => soq.SwapIt&lt;SomeDependency&gt;(It.IsAny&lt;SomeDependency&gt;()))
+        ///         .Returns(dummy);
+        ///         Soq.ConfigureForTest(mockSoq.Object);
+        /// </code>
         /// </summary>
-        /// <typeparam name="T">The type of the dependancy interface or class</typeparam>
-        /// <param name="dependency">The instantiated instance of a T. Typically your mock will simpley discard this, and return a mock object instead.</param>
-        /// <returns>Whatever you have configured your mock object to return</returns>
-        public virtual T QuoteImplementation<T>(T dependency)
+        /// <typeparam name="T">The type of the dependency interface or class</typeparam>
+        /// <param name="originalInstance">
+        /// The real instance of a T. Typically your mock will simply discard this, and return a mock object instead.
+        /// </param>
+        /// <returns>Whatever you configure your fake Soq to return</returns>
+        protected virtual T SwapIt<T>(T originalInstance) => originalInstance;
+
+        /// <summary>Allows you to inject a fake or mock or alternative Soq which you can configure to return mock objects when <seealso cref="Soq.It{T}"/> is called.</summary>
+        /// <param name="replacementSoq">Your fake or mocking Soq</param>
+        public static void SwapSoqs(Soq replacementSoq=null)
         {
-            return dependency;
+            if (IsLocked) throw new InvalidOperationException("The Soq may not be configured when ConfigurationIsEnabled is false.");
+            instance = replacementSoq ?? new Soq();
         }
 
-        /// <summary>
-        /// Allows you to inject a Moq Soq which you can configure to return mock objects when QuoteImplementation is called.
-        /// </summary>
-        /// <param name="testSoq">Your Moq Soq</param>
-        public static void ConfigureForTest(Soq testSoq)
-        {
-            if (!ConfigurationIsEnabled)
-            {
-                throw new InvalidOperationException("The Soq may not be configured when ConfigurationIsEnabled is false.");
-            }
-            instance = testSoq ?? new Soq();
-        }
-        /// <summary>
-        /// If you want to lock down your Soqs under certain conditions then implement your lockdown here. If you don't, you can delete this member.
-        /// </summary>
-        public static bool ConfigurationIsEnabled { get { return true; } }
+        /// <summary>Lock your Soqs to avoid surprises in large and sprawling codebases</summary>
+        public static void Lock() => IsLocked = true;
+        
+        protected internal static bool IsLocked =false;
     }
 }
---
+++ SoqItTestsAndExamples.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SoqIt
+{
+    [TestFixture]
+    public class WhenADependencyIsSoqIted
+    {
+        [Test]
+        public void SoqReturnsTheOriginalInstance()
+        {
+            Soq.SwapSoqs();
+            
+            var original = new SomeDependency();
+            var soqIted= Soq.It(original);
+            
+            Assert.AreSame(original,soqIted);
+        }
+    }
+    
+    [TestFixture]
+    public class WhenAFakeSoqIsUsed
+    {
+        class FakeSoq : Soq
+        {
+            protected override T SwapIt<T>(T dependency) => Fakes.ContainsKey(typeof(T)) ? (T)Fakes[typeof(T)] : base.SwapIt(dependency);
+            public FakeSoq Setup<T>(T instance){Fakes.Add(typeof(T), instance); return this;}
+            readonly Dictionary<Type,object> Fakes= new Dictionary<Type, object>();
+        }
+        
+        [Test]
+        public void SoqIt_ReturnsFakeDependencies()
+        {
+            SomeDependency someDependency = new FakeDependency();
+            
+            Soq.SwapSoqs(new FakeSoq().Setup(someDependency));
+            
+            var myClass = new MyClassThatDependsonSomething();
+            var dependency = myClass.D1;
+
+            Assert.AreSame( someDependency , myClass.D1);
+        }
+        class FakeDependency : SomeDependency{}
+    }
+
+    [TestFixture]
+    public class SoqsCanBeLocked
+    {
+        [Test]
+        public void ALockedSoqCannotBeReplaced()
+        {
+            try
+            {
+                Soq.SwapSoqs();
+                Soq.Lock();
+                Assert.Throws<InvalidOperationException>(() => Soq.SwapSoqs(new Soq()));
+            }
+            finally { Soq.IsLocked = false; }
+        }
+    }
+   
+    public class SomeDependency{}
+
+    public class OtherDependency
+    {
+        public static OtherDependency GetOtherDependency() => new OtherDependency();
+    }
+
+    public class MyClassThatDependsonSomething
+    {
+        public MyClassThatDependsonSomething()
+        {
+            D1 = Soq.It(new SomeDependency());
+            D2 = Soq.It(OtherDependency.GetOtherDependency());
+        }
+
+        internal SomeDependency D1 { get; }
+        OtherDependency D2 { get; }
+    }    
+}
